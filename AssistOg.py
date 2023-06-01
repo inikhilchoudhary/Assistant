29c839973c2f2a990b188645dea478ee072aed81
Nikhil Choudhary <94347474+inikhilchoudhary@users.noreply.github.com>
2023-06-01 11:50:25 +0530

    BackgroundImage

--- AssistOg.py
+++ AssistOg.py
@@ -9,6 +9,8 @@ import random
 import geocoder
 import requests
 import threading
+from PIL import ImageTk, Image
+
 
 listener = sr.Recognizer()
 engine = pyttsx3.init()
@@ -47,7 +49,7 @@ def find_nearby_food_shops(latitude, longitude):
             places.append(result['name'])
     return places
 
-def run_ng(output_label):
+def run_ng(output_text):
     while True:
         command = take_command()
         if command:
@@ -55,47 +57,56 @@ def run_ng(output_label):
                 current_time = datetime.datetime.now().strftime('%I:%M %p')
                 response_text = 'The current time is ' + current_time
                 talk(response_text)
-                output_label.config(text=response_text)
+                output_text.insert(END, "\nUser: " + command)
+                output_text.insert(END, "\nAssistant: " + response_text)
             elif 'who is' in command:
                 person = command.replace('who is', '')
                 try:
                     info = wikipedia.summary(person, 1)
                     response_text = info
                     talk(response_text)
-                    output_label.config(text=response_text)
+                    output_text.insert(END, "\nUser: " + command)
+                    output_text.insert(END, "\nAssistant: " + response_text)
                 except wikipedia.exceptions.DisambiguationError:
                     response_text = 'There are multiple results. Please be more specific.'
                     talk(response_text)
-                    output_label.config(text=response_text)
+                    output_text.insert(END, "\nUser: " + command)
+                    output_text.insert(END, "\nAssistant: " + response_text)
             elif 'play' in command:
                 song = command.replace('play', '')
                 response_text = 'Playing ' + song
                 talk(response_text)
-                output_label.config(text=response_text)
+                output_text.insert(END, "\nUser: " + command)
+                output_text.insert(END, "\nAssistant: " + response_text)
                 pywhatkit.playonyt(song)
             elif 'are you single' in command:
                 response_text = 'I am in a relationship with Wi-Fi'
                 talk(response_text)
-                output_label.config(text=response_text)
+                output_text.insert(END, "\nUser: " + command)
+                output_text.insert(END, "\nAssistant: " + response_text)
             elif 'joke' in command:
                 joke = pyjokes.get_joke()
                 response_text = joke
                 talk(response_text)
-                output_label.config(text=response_text)
+                output_text.insert(END, "\nUser: " + command)
+                output_text.insert(END, "\nAssistant: " + response_text)
             elif 'date' in command:
                 response_text = 'Sorry, I have a headache'
                 talk(response_text)
-                output_label.config(text=response_text)
+                output_text.insert(END, "\nUser: " + command)
+                output_text.insert(END, "\nAssistant: " + response_text)
             elif 'how are you' in command:
                 HruResponse = ['I am good, what about you?', 'I am great and you?', 'Great, thank you. How are you?', 'Fine, thanks. It\'s a beautiful day.']
                 response_text = random.choice(HruResponse)
                 talk(response_text)
-                output_label.config(text=response_text)
+                output_text.insert(END, "\nUser: " + command)
+                output_text.insert(END, "\nAssistant: " + response_text)
             elif 'what is my current location' in command:
                 location = geocoder.ip('me')
                 response_text = 'Your current location is ' + location.city
                 talk(response_text)
-                output_label.config(text=response_text)
+                output_text.insert(END, "\nUser: " + command)
+                output_text.insert(END, "\nAssistant: " + response_text)
             elif 'food' in command or 'restaurant' in command:
                 location = geocoder.ip('me')
                 user_location = location.latlng
@@ -104,31 +115,37 @@ def run_ng(output_label):
                     if places:
                         response_text = "Here are some nearby food shops or famous restaurants:\n" + "\n".join(places)
                         talk(response_text)
-                        output_label.config(text=response_text)
+                        output_text.insert(END, "\nUser: " + command)
+                        output_text.insert(END, "\nAssistant: " + response_text)
                     else:
                         response_text = "Sorry, I couldn't find any nearby food shops or famous restaurants."
                         talk(response_text)
-                        output_label.config(text=response_text)
+                        output_text.insert(END, "\nUser: " + command)
+                        output_text.insert(END, "\nAssistant: " + response_text)
                 else:
                     response_text = "Sorry, I couldn't determine your current location."
                     talk(response_text)
-                    output_label.config(text=response_text)
+                    output_text.insert(END, "\nUser: " + command)
+                    output_text.insert(END, "\nAssistant: " + response_text)
             elif 'stop' in command or 'exit' in command:
                 response_text = 'Goodbye.'
                 talk(response_text)
-                output_label.config(text=response_text)
+                output_text.insert(END, "\nUser: " + command)
+                output_text.insert(END, "\nAssistant: " + response_text)
                 break
             else:
                 response_text = 'Sorry, I did not understand that command.'
                 talk(response_text)
-                output_label.config(text=response_text)
+                output_text.insert(END, "\nUser: " + command)
+                output_text.insert(END, "\nAssistant: " + response_text)
 
             talk('Anything more?')  # Ask for further questions
             response = take_command()  # Get the user's response
             if response and ('no' in response or 'stop' in response or 'exit' in response):
                 response_text = 'Goodbye.'
                 talk(response_text)
-                output_label.config(text=response_text)
+                output_text.insert(END, "\nUser: " + response)
+                output_text.insert(END, "\nAssistant: " + response_text)
                 break
             elif response:
                 command = response
@@ -137,23 +154,38 @@ def run_ng(output_label):
                 break
 
 def on_button_click():
-    threading.Thread(target=run_ng, args=(output_label,)).start()
+    threading.Thread(target=run_ng, args=(output_text,)).start()
+
+
 
 # Create the GUI window
 T = Tk()
 T.title("HelpU")
-T.geometry("400x400")
+T.geometry("400x600")  # Adjust the window size as needed
 
 # For Icon of GUI window
 T.iconbitmap("AssistantIcon.ico")
 
-# Create a button
-button = Button(T, text="Tab To Run", command=on_button_click)
-button.pack(pady=20)
+# Load the background image
+background_image = ImageTk.PhotoImage(Image.open("WinBackIma.png"))
 
-# Create a label to display the output
-output_label = Label(T, text="")
-output_label.pack(pady=20)
+# Create a Label widget to hold the background image
+background_label = Label(T, image=background_image)
+background_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+# Create a button with Google Assistant style
+button = Button(T, text="Tap to Run", font=("Arial", 14, "bold"), bg="#4285F4", fg="white", relief=FLAT, command=on_button_click)
+button.place(relx=0.5, rely=0.1, anchor=CENTER)
+
+# Create a text widget to display the output
+output_text = Text(T, height=10, width=40, font=("Arial", 12), bd=0, wrap=WORD)
+output_text.place(relx=0.5, rely=0.3, anchor=CENTER)
+
+
+# Add a scrollbar to the output text widget
+scrollbar = Scrollbar(T, command=output_text.yview)
+scrollbar.place(relx=0.95, rely=0.45, relheight=0.4, anchor=E)
+output_text.configure(yscrollcommand=scrollbar.set)
 
 # Run the GUI event loop
 T.mainloop()
